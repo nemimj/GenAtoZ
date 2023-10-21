25afe6670062272817e4d5a858759c1b0a010778
nemi <nemimj123@gmail.com>
2023-10-21 22:36:29 +0530

    added shop.html link with product.html

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let bar = document.getElementById("bar");
 let menu = document.querySelector(".menu");
 let close = document.querySelector("#close");
+let mainImg = document.querySelector(".main-img");
+let smallImg = document.querySelectorAll(".small-img");
 
 bar.addEventListener("click", () => {
   menu.classList.add("show-menu");
@@ -9,3 +11,13 @@ bar.addEventListener("click", () => {
 close.addEventListener("click", () => {
   menu.classList.remove("show-menu");
 });
+
+smallImg[0].onclick = () => {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = () => {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = () => {
+  mainImg.src = smallImg[2].src;
+};
